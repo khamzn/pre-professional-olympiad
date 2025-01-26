001e2bf2cb134d8793fa317365ad5051ee1a3850
khamzn <polyakovrrn@gmail.com>
2025-01-26 12:53:06 +0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,31 +1,135 @@
-void setup() {
- // Начинаем работу с Serial портом на скорости 9600 бит/с
- Serial.begin(9600);
- pinMode(13, OUTPUT);
+#include <Stepper.h>
+
+#define motor1_A 1
+#define motor1_B 2
+#define motor2_A 3
+#define motor2_B 4
+
+#define redpin 5
+#define greenpin 6
+
+#define button 7
+
+Stepper stepper(200, 8, 9);
+
+int buttonState = digitalRead(button);
+
+int redpinState;
+int greenpinState;
+
+void setup(){
+  Serial.begin(9600);
+  stepper.setSpeed(1000);
+  
+  pinMode(motor1_A, OUTPUT);
+  pinMode(motor1_B, OUTPUT);
+  pinMode(motor2_A, OUTPUT);
+  pinMode(motor2_B, OUTPUT);
+
+  pinMode(button, INPUT_PULLUP);
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
 }
-void loop() {
-  if (Serial.available()>0){
-    String data = Serial.readStringUntil('\n');
-    
-    if (data == "loading"){
-      load();
-    }
-    if (data == "unloading"){
-      unload();
-    }
+
+void loop(){
+  buttonState = digitalRead(button);
+
+  if (Serial.available() > 0) {
+    data = Serial.readString();
   }
+
+  if (data == "loading"){
+    loading();
+  }
+
+  if (data == "unloading"){
+    unloading();
+  }
+
+  if (data == "red"){
+    redpinState = 1;
+    greenpinState = 0;
+  }
+  if (data == "green"){
+    redpinState = 0;
+    greenpinState = 1;
+  }
+
+  digtalWrite(redpin, redpinState);
+  digtalWrite(greenpin, greenpinState);
 }
 
-void load(){
-  digitalWrite(13, HIGH);
+void loading(){
+  while (buttonState == 1){
+    digitalWrite(redpin, 1);
+    digitalWrite(greenpin, 0);
+  }
+  digitalWrite(redpin, 1);
+  digitalWrite(greenpin, 0);
+
+  digitalWrite(motor1_A, 1);
+  digitalWrite(motor1_B, 0);
   delay(1000);
-  digitalWrite(13, LOW);
+
+  digitalWrite(motor1_A, 0);
+  digitalWrite(motor2_A, 1);
+  digitalWrite(motor2_B, 0);
+  delay(3000);
+
+  digitalWrite(motor2_A, 0);
+  digitalWrite(motor1_A, 0);
+  digitalWrite(motor1_B, 1);
+  delay(3000);
+
+  digitalWrite(motor1_B, 0);
+  digitalWrite(motor2_A, 0);
+  digitalWrite(motor2_B, 1);
+  delay(3000);
+
+  digitalWrite(motor2_B, 0);
+  digitalWrite(motor1_A, 1);
+  digitalWrite(motor1_B, 0);
   delay(1000);
+
+  digitalWrite(motor1_A, 0);
+
+  Serial.println("loading end")
+  digitalWrite(redpin, 0);
+  digitalWrite(greenpin, 1);
 }
 
-void unload(){
-  digitalWrite(13, HIGH);
+
+void unloading(){
+  digitalWrite(redpin, 1);
+  digitalWrite(greenpin, 0);
+
+  digitalWrite(motor1_A, 1);
+  digitalWrite(motor1_B, 0);
+  delay(1000);
+
+  digitalWrite(motor1_A, 0);
+  digitalWrite(motor2_A, 1);
+  digitalWrite(motor2_B, 0);
   delay(3000);
-  digitalWrite(13, LOW);
+
+  digitalWrite(motor2_A, 0);
+  digitalWrite(motor1_A, 0);
+  digitalWrite(motor1_B, 1);
   delay(3000);
+
+  digitalWrite(motor1_B, 0);
+  digitalWrite(motor2_A, 0);
+  digitalWrite(motor2_B, 1);
+  delay(3000);
+
+  digitalWrite(motor2_B, 0);
+  digitalWrite(motor1_A, 1);
+  digitalWrite(motor1_B, 0);
+  delay(1000);
+
+  digitalWrite(motor1_A, 0);
+
+  Serial.println("unloading end")
+  digitalWrite(redpin, 0);
+  digitalWrite(greenpin, 1);
 }
